0087e00617df703045dda76cf8ad88cb355ef11a
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-04-10 12:55:55 -0300

    Add files via upload
    servidor do formulario do portfolio

---
+++ index.js
@@ -0,0 +1,69 @@
+// CODIGOS DE INSTALACAO
+// npm i express
+// npm install --save-dev nodemon
+// npm i nodemailer dotenv
+// npm install --save body-parser
+
+
+const express = require('express')
+const nodemailer = require('nodemailer')
+const bodyParser = require('body-parser')
+
+// npm install --save-dev sucrase****
+// import mensagemCliente from './mensagemCliente' // teste
+// console.log(mensagemCliente)
+
+const app = express()
+require('dotenv').config()
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+
+
+app.get('/', (req, res) => res.send('deu certo'))
+
+app.post('/enviarEmail', (req, res) => {
+
+    const email = req.body.email
+    const mensagem = req.body.mensagem
+
+    const transport = nodemailer.createTransport({
+        host: 'smtp.office365.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.SENHA
+        }
+    })
+
+    transport.sendMail({
+        from: `enviado de <${process.env.EMAIL}>`,
+        to: 'joseelguesabal@gmail.com',
+        subject: 'Nova mensagem da caixa do portfolio',
+        html: `<h1>Email de contato: <br/> ${email}</h1> <p>${mensagem}</p>`,
+        text: `Email de contato: ${email} ${mensagem}`
+    })
+    .then(() => console.log('email enviado'))
+    .catch((err) => console.log('deu esse erro ai', err))
+
+
+    transport.sendMail({
+        from: `enviado de <${process.env.EMAIL}>`,
+        to: email,
+        subject: 'Obrigado por me enviar uma mensagem!',
+        html: `<p>Agradeço o contato e em breve estarei respondendo!</p>`,
+        text: `Agradeço o contato e em breve estarei respondendo!`
+    })
+    .then(() => console.log('email respondido'))
+    .catch((err) => console.log('deu esse erro ai', err))
+
+
+    res.send('email enviado')
+})
+
+
+
+const port = 3000
+app.listen(process.env.PORT || port, () => console.log(`servidor rodando na porta ${port}`), console.log(`acesse o link http://localhost:${port}`))
