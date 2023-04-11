3889f781066255f59bb26a721fc126279f7151e6
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-04-11 00:13:30 -0300

    Add files via upload
    email para o cliente temporariamente desativado kkkkk

--- index.js
+++ index.js
@@ -50,18 +50,15 @@ app.post('/enviarEmail', (req, res) => {
     .then((resposta) => res.send(`email enviado para joseelguesabal@gmail.com`))
     .catch((erro) => res.send(`o email para joseelguesabal@gmail.com nao foi enviado <br/> ${erro}`))
 
-
-    setTimeout( function() {
-        transport.sendMail({
-            from: `enviado de <${process.env.EMAIL}>`,
-            to: email,
-            subject: 'Obrigado por me enviar uma mensagem!',
-            html: `<p>Agradeço o contato e em breve estarei respondendo em breve!</p>`,
-            text: `Agradeço o contato e em breve estarei respondendo!`
-        })
-        .then((resposta) => res.send(`email enviado para ${email}`))
-        .catch((erro) => res.send(`o email para ${email} nao foi enviado`))    
-    }, 10000)
+    // transport.sendMail({
+    //     from: `enviado de <${process.env.EMAIL}>`,
+    //     to: email,
+    //     subject: 'Obrigado por me enviar uma mensagem!',
+    //     html: `<p>Agradeço o contato e em breve estarei respondendo em breve!</p>`,
+    //     text: `Agradeço o contato e em breve estarei respondendo!`
+    // })
+    // .then((resposta) => res.send(`email enviado para ${email}`))
+    // .catch((erro) => res.send(`o email para ${email} nao foi enviado`))    
 })
 
 
