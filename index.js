50aece65642870a78555d4da5682d3593f0e0648
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-04-10 22:43:57 -0300

    Add files via upload
    agr vou testar com 1 email so

--- index.js
+++ index.js
@@ -49,15 +49,15 @@ app.post('/enviarEmail', (req, res) => {
     .catch((erro) => res.send(`o email para joseelguesabal@gmail.com nao foi enviado`))
 
 
-    transport.sendMail({
-        from: `enviado de <${process.env.EMAIL}>`,
-        to: email,
-        subject: 'Obrigado por me enviar uma mensagem!',
-        html: `<p>Agradeço o contato e em breve estarei respondendo em breve!</p>`,
-        text: `Agradeço o contato e em breve estarei respondendo!`
-    })
-    .then((resposta) => res.send(`email enviado para ${email}`))
-    .catch((erro) => res.send(`o email para ${email} nao foi enviado`))    
+    // transport.sendMail({
+    //     from: `enviado de <${process.env.EMAIL}>`,
+    //     to: email,
+    //     subject: 'Obrigado por me enviar uma mensagem!',
+    //     html: `<p>Agradeço o contato e em breve estarei respondendo em breve!</p>`,
+    //     text: `Agradeço o contato e em breve estarei respondendo!`
+    // })
+    // .then((resposta) => res.send(`email enviado para ${email}`))
+    // .catch((erro) => res.send(`o email para ${email} nao foi enviado`))    
 })
 
 
