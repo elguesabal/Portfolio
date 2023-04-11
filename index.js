7460d4df06662d17aece31cbd3015d46ecebb85c
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-04-10 23:59:56 -0300

    Add files via upload
    voltando a estaca zero kkkk

--- index.js
+++ index.js
@@ -19,9 +19,10 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 
-                    app.get('/teste', (req, res) => res.send('so pra testar'))
 
 app.get('/', (req, res) => res.send('deu certo <a href="/teste">clica aki pra redirecionar</a>'))
+app.get('/teste', (req, res) => res.send('so pra testar'))
+
 
 app.post('/enviarEmail', (req, res) => {
 
@@ -38,6 +39,7 @@ app.post('/enviarEmail', (req, res) => {
         }
     })
 
+
     transport.sendMail({
         from: `enviado de <${process.env.EMAIL}>`,
         to: 'joseelguesabal@gmail.com',
@@ -46,18 +48,17 @@ app.post('/enviarEmail', (req, res) => {
         text: `Email de contato: ${email} ${mensagem}`
     })
     .then((resposta) => res.send(`email enviado para joseelguesabal@gmail.com`))
-    .catch((erro) => res.send(`o email para joseelguesabal@gmail.com nao foi enviado`))
+    .catch((erro) => res.send(`o email para joseelguesabal@gmail.com nao foi enviado <br/> ${erro}`))
 
-
-    // transport.sendMail({
-    //     from: `enviado de <${process.env.EMAIL}>`,
-    //     to: email,
-    //     subject: 'Obrigado por me enviar uma mensagem!',
-    //     html: `<p>Agradeço o contato e em breve estarei respondendo em breve!</p>`,
-    //     text: `Agradeço o contato e em breve estarei respondendo!`
-    // })
-    // .then((resposta) => res.send(`email enviado para ${email}`))
-    // .catch((erro) => res.send(`o email para ${email} nao foi enviado`))    
+    transport.sendMail({
+        from: `enviado de <${process.env.EMAIL}>`,
+        to: email,
+        subject: 'Obrigado por me enviar uma mensagem!',
+        html: `<p>Agradeço o contato e em breve estarei respondendo em breve!</p>`,
+        text: `Agradeço o contato e em breve estarei respondendo!`
+    })
+    .then((resposta) => res.send(`email enviado para ${email}`))
+    .catch((erro) => res.send(`o email para ${email} nao foi enviado`))    
 })
 
 
