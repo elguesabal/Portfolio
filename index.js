ac7f0433e336b4e6381f5717fc40c06cf15173fb
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-04-11 00:07:07 -0300

    Add files via upload
    meti um setTimeout pra ver se funfa melhor

--- index.js
+++ index.js
@@ -50,15 +50,18 @@ app.post('/enviarEmail', (req, res) => {
     .then((resposta) => res.send(`email enviado para joseelguesabal@gmail.com`))
     .catch((erro) => res.send(`o email para joseelguesabal@gmail.com nao foi enviado <br/> ${erro}`))
 
-    transport.sendMail({
-        from: `enviado de <${process.env.EMAIL}>`,
-        to: email,
-        subject: 'Obrigado por me enviar uma mensagem!',
-        html: `<p>Agradeço o contato e em breve estarei respondendo em breve!</p>`,
-        text: `Agradeço o contato e em breve estarei respondendo!`
-    })
-    .then((resposta) => res.send(`email enviado para ${email}`))
-    .catch((erro) => res.send(`o email para ${email} nao foi enviado`))    
+
+    setTimeout( function() {
+        transport.sendMail({
+            from: `enviado de <${process.env.EMAIL}>`,
+            to: email,
+            subject: 'Obrigado por me enviar uma mensagem!',
+            html: `<p>Agradeço o contato e em breve estarei respondendo em breve!</p>`,
+            text: `Agradeço o contato e em breve estarei respondendo!`
+        })
+        .then((resposta) => res.send(`email enviado para ${email}`))
+        .catch((erro) => res.send(`o email para ${email} nao foi enviado`))    
+    }, 10000)
 })
 
 
