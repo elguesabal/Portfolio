7e6fe49f9406a5e6fb38949151d4dd6f2601698e
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-04-12 14:09:34 -0300

    Add files via upload
    pagina de mensagem enviada adicionada

---
+++ src/paginas/mensagem enviada.jsx
@@ -0,0 +1,20 @@
+function MensagemEnviada() {
+
+    return (
+        <div class="telaMensagem">
+            <div className="painelMensagem">
+                <div>
+                    <div class="d-flex justify-content-center">
+                        <h2>Mensagem enviada com sucesso!</h2>
+                    </div>
+                    <br /><br />
+                    <div class="d-flex justify-content-center">
+                        <a href="/" class="w-100 btn btn-lg btn-outline-light">Voltar para página inicial</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MensagemEnviada
