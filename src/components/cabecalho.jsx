65f3812779ee88d9ba2eade222ede11336d85e6d
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-04-13 14:04:35 -0300

    Add files via upload
    adicionado botao back to up no canto direito inferior

--- src/components/cabecalho.jsx
+++ src/components/cabecalho.jsx
@@ -19,14 +19,6 @@ function Cabecalho() {
                 </div>
                 <div class="menu">
                     <ul>
-                        <a href="#topo-site">
-                            <li>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-                                </svg>
-                                Topo
-                            </li>
-                        </a>
                         <a href="#card-perfil">
                             <li>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-vcard" viewBox="0 0 16 16">
