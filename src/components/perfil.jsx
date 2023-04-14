78d66c410b326b1b19d5a7c206e4e6911ce80994
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-04-13 22:35:13 -0300

    Add files via upload
    adicionado curriculo

--- src/components/perfil.jsx
+++ src/components/perfil.jsx
@@ -41,6 +41,7 @@ function Perfil() {
                                 <img class="bd-placeholder-img rounded-circle " width="140" height="140" src="https://media.licdn.com/dms/image/D4D03AQE0ANrEe4uO7w/profile-displayphoto-shrink_200_200/0/1665860003192?e=1685577600&v=beta&t=tpW4Zae687TRYNVYc-QyixbwPN4ik8-C5x7ki6MgnUk" alt="" />
                                 <h2 class="fw-normal nomePerfil">José</h2>
                                 <p>Desenvolvedor web Full Stack</p>
+                                <a class="btn btn-lg btn-outline-light" href="https://www.canva.com/design/DAFTzR3VmGY/qLmFQigCW6Y8hRZ8Ly7gew/view?utm_content=DAFTzR3VmGY&utm_campaign=share_your_design&utm_medium=link&utm_source=shareyourdesignpanel" target="_blank">Currículo</a>
                             </div>
                         </div>
                     </div>
